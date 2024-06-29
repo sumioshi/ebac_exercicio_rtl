c6c3380e36a704e03aac67450cef78b666410f5d
Rodrigo Sumioshi <43658223+sumioshi@users.noreply.github.com>
2024-06-29 11:15:31 -0300

    css to syled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import styled from 'styled-components';
 import Post from "./components/Post";
 
-import styles from './App.module.css';
+const AppContainer = styled.div`
+    max-width: 480px;
+    margin: 120px auto;
+`;
 
 function App() {
   return (
-    <div className={styles.app}>
-      <Post imageUrl="https://www.orangeboxminiaturas.com.br/img/products/batmovel-1989-figura-batman-em-metal-jada-toys-1-24-jad-98260_1_1000.jpg">
+    <AppContainer>
+      <Post imageUrl="https://fotos-jornaldocarro-estadao.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2020/06/04123612/EZWqtbbXgAEaxSC.jpg">
         Olha só que legal minha miniatura do Batmóvel.
       </Post>
-    </div>
+    </AppContainer>
   );
 }
 
--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,19 +1,38 @@
-import styles from './Post.module.css';
-
 import PostComments from '../PostComments';
 import { ReactNode } from 'react';
+import styled from 'styled-components';
 
 type Props = {
     children: ReactNode;
     imageUrl: string;
 }
 
+const PostContainer = styled.div`
+    background-color: #fff;
+    padding: 16px;
+    box-shadow: 1px 2px 6px #333;
+    border-radius: 16px;
+`;
+
+const PostImage = styled.img`
+    max-width: 100%;
+    display: block;
+`
+
+const PostText = styled.p`
+    color: #333;
+    font-size: 14px;
+    line-height: 22px;
+    margin-bottom: 24px;
+`
+
+
 const Post = ({ children, imageUrl }: Props) => (
-    <div className={styles.post}>
-        <img className={styles['post-image']} src={imageUrl} />
-        <p className={styles['post-text']}> {children} </p>
+    <PostContainer>
+        <PostImage src={imageUrl} />
+        <PostText>{children}</PostText>
         <PostComments />
-    </div>
+    </PostContainer>
 );
 
 export default Post;
